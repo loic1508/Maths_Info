b3d5a2fac45a8699a929eef7165ff9ffea08fc6d
loic1508 <loicdelobel15@gmail.com>
2026-04-27 14:09:45 +0200

    Les boucles

--- tp3.py
+++ tp3.py
@@ -1,15 +1,7 @@
-x = 15
-if x > 10:
-    print("grand")
-else:
-    print("petit")
+for i in range(5):
+    print(i)
 
-note = 15
-if note >= 18:
-    print("Excellent")
-elif note >= 14:
-    print("Bien")
-elif note >= 10:
-    print("Passable")
-else:
-    print("Échec")
+x = 0
+while x < 10:
+    x += 1
+print(x)
