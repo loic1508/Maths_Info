4b5fa59013750d7067a1a38955aa3198345c20c2
loic1508 <loicdelobel15@gmail.com>
2026-04-27 13:58:29 +0200

    EX1

---
+++ tp3.py
@@ -0,0 +1,16 @@
+x = 15
+
+if x > 10:
+    print("grand")
+else:
+    print("petit")
+
+note = 15
+if note >= 18:
+    print("Excellent")
+elif note >= 14:
+    print("Bien")
+elif note >= 10:
+    print("Passable")
+else:
+    print("Échec")
